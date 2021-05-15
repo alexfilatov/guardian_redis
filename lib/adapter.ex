027ad80f3d7106e7235af1c8d01e91ea51e70a7f
Alex Filatov <alex@alexfilatov.com>
2021-05-15 12:27:39 +0100

    updated adapter according to https://github.com/ueberauth/guardian_db/pull/127

--- lib/adapter.ex
+++ lib/adapter.ex
@@ -4,20 +4,38 @@ defmodule Guardian.DB.Adapter do
 
   This behaviour allows to use any storage system
   for Guardian Tokens.
-
-  TODO: remove adapter from this package after it will be approved here https://github.com/ueberauth/guardian_db/pull/126
   """
 
   @typep query :: Ecto.Query.t()
   @typep schema :: Ecto.Schema.t()
-  @typep schema_or_changeset :: schema() | Ecto.Changeset.t()
+  @typep changeset :: Ecto.Changeset.t()
+  @typep schema_or_changeset :: schema() | changeset()
   @typep queryable :: query() | schema()
   @typep opts :: keyword()
   @typep id :: pos_integer() | binary() | Ecto.UUID.t()
 
-  @callback one(queryable()) :: nil | schema()
-  @callback insert(schema_or_changeset()) :: {:ok, schema()}
-  @callback delete(schema_or_changeset()) :: {:ok, schema()}
-  @callback delete_all(queryable()) :: {:ok, pos_integer()}
-  @callback delete_all(queryable(), opts()) :: {:ok, pos_integer()}
+  @doc """
+  Retrieves JWT token
+  Used in `Guardian.DB.Token.find_by_claims/1`
+  """
+  @callback one(queryable()) :: schema() | nil
+
+  @doc """
+  Persists JWT token
+  Used in `Guardian.DB.Token.create/2`
+  """
+  @callback insert(schema_or_changeset()) :: {:ok, schema()} | {:error, changeset()}
+
+  @doc """
+  Deletes JWT token
+  Used in `Guardian.DB.Token.destroy_token/3`
+  """
+  @callback delete(schema_or_changeset(), opts()) :: {:ok, schema()} | {:error, changeset()}
+
+  @doc """
+  Purges all JWT tokens
+  Used in `Guardian.DB.Token.purge_expired_tokens/0 and in `Guardian.DB.Token.destroy_by_sub/1`
+  Returns a tuple containing the number of entries and any returned result as second element.
+  """
+  @callback delete_all(queryable(), opts()) :: {integer(), nil | [term()]}
 end
