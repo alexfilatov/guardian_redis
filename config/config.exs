ac565349a96f8bc5ffc756809548c89729c8e953
Max Strother <max@confly.pl>
2023-10-02 16:24:45 +0200

    Remove outdated config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,6 @@
 use Mix.Config
 
-config :guardian, Guardian.DB,
-  # Add your repository module
-  repo: GuardianRedis.Repo,
-  # default: 60 minutes
-  sweep_interval: 60
-
+config :guardian, Guardian.DB, repo: GuardianRedis.Repo
 # store all token types if not set
 # token_types: ["refresh_token"],
 
